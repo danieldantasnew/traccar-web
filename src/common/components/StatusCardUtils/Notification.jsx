9d5bbf6263a0add6b1ff749961613e2a86b617aa
Daniel Dantas <xxxxxdanielzimmxxxx.danieldant@gmail.com>
2025-05-27 14:48:20 -0300

    Melhorando o sistema de ativação de notificações

--- src/common/components/StatusCardUtils/Notification.jsx
+++ src/common/components/StatusCardUtils/Notification.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import { faBell as faBellRegular } from "@fortawesome/free-regular-svg-icons";
+import { useDispatch, useSelector } from "react-redux";
+import { useCatch } from "../../../reactHelper";
+import { MenuItem, Typography } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Notification = () => {
+const Notification = ({ label, color, styleRow, notificationType }) => {
+  const dispatch = useDispatch();
+  const selectedId = useSelector((state) => state.devices.selectedId);
+
+  const activeNotification = useCatch(async (type) => {
+    const response = await fetch(`/api/notifications`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        type,
+        always: true,
+        notificators: "web",
+        calendarId: 0,
+        attributes: {},
+      }),
+    });
+    if (response.ok) {
+      const json = await response.json();
+      const responsePermission = await fetch("/api/permissions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          deviceId: selectedId,
+          notificationId: json.id,
+        }),
+      });
+
+      if (!responsePermission.ok) {
+        const errorText = await responsePermission.text();
+        throw new Error(
+          errorText || `Erro na permissão: ${responsePermission.status}`
+        );
+      }
+    } else {
+      throw Error(await response.text());
+    }
+  });
+
+  const removeNotification = useCatch(async (notificationId) => {
+    const response = await fetch("/api/permissions", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        deviceId: selectedId,
+        notificationId,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(await response.text());
+    }
+  });
+
   return (
-    <div>Notification</div>
-  )
-}
+    <MenuItem sx={styleRow} onClick={() => activeNotification(notificationType)}>
+      <FontAwesomeIcon
+        icon={faBellRegular}
+        style={{ height: "22px", width: "20px" }}
+        color={`${color}`}
+      />
+      <Typography>{label}</Typography>
+    </MenuItem>
+  );
+};
 
-export default Notification
+export default Notification;
--- src/common/components/StatusCardUtils/Notifications.jsx
+++ src/common/components/StatusCardUtils/Notifications.jsx
@@ -1,107 +1,40 @@
 import { faBell, faCaretDown } from "@fortawesome/free-solid-svg-icons";
-import { faBell as faBellRegular } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Menu, MenuItem, Typography } from "@mui/material";
 import { useState } from "react";
-import { DynamicIconsComponent } from "../DynamicIcons";
-import { useCatch } from "../../../reactHelper";
-import { useDispatch, useSelector } from "react-redux";
+import Notification from "./Notification";
+
+const notifications = [
+  {
+    label: "Avise-me quando ligar",
+    notificationType: "ignitionOn",
+  },
+  {
+    label: "Avise-me quando desligar",
+    notificationType: "ignitionOff",
+  },
+  {
+    label: "Avise-me quando estiver em movimento",
+    notificationType: "deviceMoving",
+  },
+  {
+    label: "Avise-me quando estiver parado",
+    notificationType: "deviceStopped",
+  },
+  {
+    label: "Avise-me quando entrar na cerca virtual",
+    notificationType: "geofenceEnter",
+  },
+  {
+    label: "Avise-me quando sair da cerca virtual",
+    notificationType: "geofenceExit",
+  },
+];
 
 const Notifications = ({ color, styleRow }) => {
   const [arrowNotification, setArrowNotification] = useState(false);
   const [anchorElNotification, setAnchorElNotification] = useState(null);
   const openNotifications = Boolean(anchorElNotification);
-
-  const dispatch = useDispatch();
-  const selectedId = useSelector((state) => state.devices.selectedId);
-
-  const activeNotification = useCatch(async (type) => {
-    const response = await fetch(`/api/notifications`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        id: selectedId,
-        type,
-        always: true,
-        notificators: "web",
-        calendarId: 0,
-        attributes: {},
-      }),
-    });
-    if (response.ok) {
-      const json = await response.json();
-      const responsePermission = await fetch("/api/permissions", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          deviceId: selectedId,
-          notificationId: json.id,
-        }),
-      });
-
-      if (!responsePermission.ok) {
-        const errorText = await responsePermission.text();
-        throw new Error(
-          errorText || `Erro na permissão: ${responsePermission.status}`
-        );
-      }
-    } else {
-      throw Error(await response.text());
-    }
-  });
-
-  const handleLink = useCatch(async () => {
-    setModalSelectDriver(false);
-    try {
-      const selectedDriver = drivers[driverSelect];
-      if (!selectedDriver || !device) return;
-
-      const responsePermission = await fetch("/api/permissions", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          deviceId: device.id,
-          driverId: selectedDriver.id,
-        }),
-      });
-
-      if (!responsePermission.ok) {
-        const errorText = await responsePermission.text();
-        throw new Error(
-          errorText || `Erro na permissão: ${responsePermission.status}`
-        );
-      }
-
-      const responseUpdate = await fetch(`/api/devices/${device.id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          ...device,
-          attributes: {
-            ...(device.attributes || {}),
-            driverUniqueId: Number(selectedDriver.uniqueId),
-          },
-        }),
-      });
-
-      if (!responseUpdate.ok) {
-        const errorText = await responseUpdate.text();
-        throw new Error(
-          errorText || `Erro ao atualizar device: ${responseUpdate.status}`
-        );
-      }
-      handleUpdateDevice();
-    } catch (error) {
-      console.error("Erro ao associar motorista:", error);
-      throw error;
-    }
-  });
 
   return (
     <>
@@ -146,72 +79,15 @@ const Notifications = ({ color, styleRow }) => {
         anchorOrigin={{ vertical: "top", horizontal: "right" }}
         transformOrigin={{ vertical: "top", horizontal: "left" }}
       >
-        <MenuItem
-          sx={styleRow}
-          onClick={() => activeNotification("ignitionOn")}
-        >
-          <FontAwesomeIcon
-            icon={faBellRegular}
-            style={{ height: "22px", width: "20px" }}
-            color={`${color}`}
+        {notifications.map((notification, index) => (
+          <Notification
+            key={'n'+ index}
+            label={notification.label}
+            color={color}
+            styleRow={styleRow}
+            notificationType={notification.notificationType}
           />
-          <Typography>Avise-me quando ligar</Typography>
-        </MenuItem>
-        <MenuItem
-          sx={styleRow}
-          onClick={() => activeNotification("ignitionOff")}
-        >
-          <FontAwesomeIcon
-            icon={faBellRegular}
-            style={{ height: "22px", width: "20px" }}
-            color={`${color}`}
-          />
-          <Typography>Avise-me quando desligar</Typography>
-        </MenuItem>
-        <MenuItem
-          sx={styleRow}
-          onClick={() => activeNotification("deviceMoving")}
-        >
-          <FontAwesomeIcon
-            icon={faBellRegular}
-            style={{ height: "22px", width: "20px" }}
-            color={`${color}`}
-          />
-          <Typography>Avise-me quando estiver em movimento</Typography>
-        </MenuItem>
-        <MenuItem
-          sx={styleRow}
-          onClick={() => activeNotification("deviceStopped")}
-        >
-          <FontAwesomeIcon
-            icon={faBellRegular}
-            style={{ height: "22px", width: "20px" }}
-            color={`${color}`}
-          />
-          <Typography>Avise-me quando estiver parado</Typography>
-        </MenuItem>
-        <MenuItem
-          sx={styleRow}
-          onClick={() => activeNotification("geofenceEnter")}
-        >
-          <DynamicIconsComponent
-            category={"bellRing"}
-            style={{ width: "20px" }}
-            color={`${color}`}
-          />
-          <Typography>Avise-me quando entrar na cerca virtual</Typography>
-        </MenuItem>
-        <MenuItem
-          sx={styleRow}
-          onClick={() => activeNotification("geofenceExit")}
-        >
-          <DynamicIconsComponent
-            category={"bellRing"}
-            style={{ width: "20px" }}
-            color={`${color}`}
-          />
-          <Typography>Avise-me quando sair da cerca virtual</Typography>
-        </MenuItem>
+        ))}
       </Menu>
     </>
   );
