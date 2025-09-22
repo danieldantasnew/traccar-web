5e21d5bed05005f98f75f4d7b0ce7b4f753e563c
Daniel Dantas <xxxxxdanielzimmxxxx.danieldant@gmail.com>
2025-09-22 16:58:40 -0300

    feat: novo efeito de transição do mapa

--- src/common/util/centerInMap.js
+++ src/common/util/centerInMap.js
@@ -1,13 +1,20 @@
 import { map } from "../../map/core/MapView";
 import dimensions from "../theme/dimensions";
 
-export default function centerInMap(position, zoomLevel, isCoordinates = false) {
+export default function centerInMap(
+  position,
+  zoomLevel,
+  isCoordinates = false
+) {
   const longitude = position.lng ? position.lng : position.longitude;
   const latitude = position.lat ? position.lat : position.latitude;
-  
-  map.easeTo({
+
+  map.flyTo({
     center: isCoordinates ? isCoordinates : [longitude, latitude],
     zoom: zoomLevel ? zoomLevel : 18,
     offset: [0, -dimensions.popupMapOffset / 2],
+    speed: 1,
+    curve: 2,
+    essential: true,
   });
 }
